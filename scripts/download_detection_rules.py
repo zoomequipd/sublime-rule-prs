61ec43fed51fc11a8799f7c657d47c0f77dbd9a5
Brandon Murphy <4827852+zoomequipd@users.noreply.github.com>
2024-10-17 15:45:51 -0500

    Update download_detection_rules.py

--- scripts/download_detection_rules.py
+++ scripts/download_detection_rules.py
@@ -439,7 +439,7 @@ def handle_open_prs():
                 # finally save it
                 # include the pr number in the filename to avoid duplicates
                 # use os.path.basename to drop the folder of the file, save_file uses OUTPUT_FOLDER anyway.
-                save_file(f"{pr['number']}_{os.path.basename(file['filename']}", content)
+                save_file(f"{pr['number']}_{os.path.basename(file['filename'])}", content)
                 new_files.add(os.path.basename(f"{pr['number']}_{file['filename']}"))
                 print(f"\tSaved: {pr['number']}_{file['filename']}")
             
